ad46b0f058db455b9b261b4d75bd1df520ac16f1
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-05-17 21:52:01 +0200

    workflow error if fork can't get merged

--- merge.sh
+++ merge.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-curl -L \
+res=$(curl --write-out %{http_code} -L -fsS \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${token}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/Archtec-io/${repo}/merge-upstream \
   -d '{"branch":"'${branch}'"}'
+)
+
+if [[ "$res" == 200 ]]; then
+  echo HTTP code $res
+  exit 0
+else
+  echo HTTP code $res
+  exit 1
+fi
